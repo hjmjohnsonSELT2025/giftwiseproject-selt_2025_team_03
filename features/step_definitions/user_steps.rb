f69a3b1be672f0cf93646d02963cefe5fca69bdd
Anel Vukovic <vukovc@uiowa.edu>
2025-11-14 18:55:48 -0600

    some login tests as well as some register page tests

---
+++ features/step_definitions/user_steps.rb
@@ -0,0 +1,63 @@
+Given("I am on the login page") do
+  visit login_path
+end
+
+Then("I should see the button: login, register") do
+  expect(page).to have_button("Login")
+  expect(page).to have_link("Register")
+end
+
+Then("I should see the fillable field: username, password") do
+  expect(page).to have_field("username")
+  expect(page).to have_field("password")
+end
+
+When("I fill in the fillable field: username, password") do
+  fill_in "username", with: "testuser"
+  fill_in "password", with: "Password1!"
+end
+
+When("I click the button: login") do
+  click_button "Login"
+end
+
+When("I click the button: register") do
+  click_link "Register"
+end
+
+Then("I should see the {string} page") do |page_name|
+  case page_name.downcase
+  when "registration"
+    expect(current_path).to eq(new_user_path)
+  when "dashboard"
+    expect(current_path).to eq(dashboard_path)
+  else
+    raise "Unknown page #{page_name}"
+  end
+end
+
+Given ("I am on the registration page") do
+  visit new_user_path
+end
+
+When("I fill in the registration form with valid details") do
+  fill_in "Username", with: "testuser"
+  fill_in "Password", with: "example@example.com"
+  fill_in "Password", with: "Password1!"
+end
+
+WHen("I click the button: Create Account") do
+  click_button "Create Account"
+end
+
+Then("I should see the flash message: {string}") do |msg|
+  expect(page).to have_content(msg)
+end
+
+Then("I should be redirected to {string} page") do |page_name|
+  case page_name.downcase
+  when "dashboard"
+    expect(current_path).to eq(dashboard_path)
+end
+
+end
