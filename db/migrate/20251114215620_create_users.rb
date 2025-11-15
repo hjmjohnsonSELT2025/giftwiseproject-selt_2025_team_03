61fba8f27d0ff7ed3834372b6689b638a90c6530
Sullivan Bryant <sbryant1@uiowa.edu>
2025-11-14 18:38:03 -0600

    (wip) adding create_users.rb

---
+++ db/migrate/20251114215620_create_users.rb
@@ -0,0 +1,14 @@
+class CreateUsers < ActiveRecord::Migration[7.1]
+  def change
+    create_table :users do |t|
+      t.string :username
+      t.string :email
+      t.string :password_digest
+      t.string :first_name
+      t.string :last_name
+      t.date :birthday
+
+      t.timestamps
+    end
+  end
+end
