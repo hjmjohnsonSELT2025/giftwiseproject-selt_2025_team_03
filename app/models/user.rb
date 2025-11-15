09d4af072e1f3300e5e9aafc2a90634dbbeec964
bkellison <blakekellison07@gmail.com>
2025-11-14 21:42:29 -0600

    Add RSpec tests for users and recipients and user controller

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,7 @@
 
 class User < ApplicationRecord
   has_secure_password
+  has_many :recipients, dependent: :destroy
   validates :username, presence: true, uniqueness: true
   validates :email, presence: true, uniqueness: true
 
