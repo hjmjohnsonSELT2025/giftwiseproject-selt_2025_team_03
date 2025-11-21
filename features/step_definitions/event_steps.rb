e5df2265a09d8f8cd3e1db2b17e2d2099b4b46b3
Anel Vukovic <vukovc@uiowa.edu>
2025-11-21 12:00:24 -0600

    Finished the creating events tests.

--- features/step_definitions/event_steps.rb
+++ features/step_definitions/event_steps.rb
@@ -32,4 +32,74 @@ When("I fill in {string} with {string}") do |field, value|
 end
 
 #adding and removing the recipients
+When("I add a recipient named {string}") do |name|
+  #click Add Recipient button
+  click_button "Add Recipient"
 
+  #fills in the most recently added row
+  within(all(".recipient-selector").last) do
+    fill_in "Recipient Name", with: name
+  end
+end
+
+Then("I should see {string} listed as a selected recipient") do |name|
+  within("#recipient-list") do
+    expect(page).to have_content(name)
+  end
+end
+
+When("I remove the recipient named {string}") do |name|
+  within("#recipient-list") do
+    item = find(".recipient-item", text: name)
+    item.find("button", text: "Remove").click
+  end
+end
+
+Then("I should not see {string} listed as a selected recipient") do |name|
+  within("#recipient-list") do
+    expect(page).to have_no_content(name)
+  end
+end
+
+#the text box that allows user to enter some extra info
+Then("I should see a text box {string}") do |label|
+  expect(page).to have_field(label)
+end
+
+#for the checkbox
+Then("I should see a checkbox {string}") do |label|
+  expect(page).to have_unchecked_field(label)
+end
+
+When("I check {string}") do |label|
+  check(label)
+end
+
+#buttons
+Then("I should see a button {string}") do |button|
+  expect(page).to have_button(button)
+end
+
+When("I click the button {string}") do |button|
+  click_button button
+end
+
+#page navigation validation
+Then("I should be taken to the {string} page") do |page_name|
+  expected_path =
+    case page_name
+    when "Events"
+      events_path
+    when "New Event"
+      new_event_path
+    else
+      raise "Unknown page name: #{page_name}"
+    end
+
+  expect(current_path).to eq(expected_path)
+end
+
+#validation errors
+Then("I should see a validation error for {string}") do |field|
+  expect(page).to have_content("#{field} can't be blank")
+end
