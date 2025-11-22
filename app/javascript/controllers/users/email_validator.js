ed94301b5c04490cd7f6a73d9243a7025af3387d
Sullivan Bryant <sbryant1@uiowa.edu>
2025-11-22 13:19:40 -0600

    (wip) Finished validating required fields, not tested. implementing validation of email js.

--- app/javascript/controllers/users/email_validator.js
+++ app/javascript/controllers/users/email_validator.js
@@ -1,3 +1,4 @@
+/*
 console.log("email validator loaded.");
 
 $(function() {
@@ -47,4 +48,53 @@ $(function() {
             infoField.text("Invalid email address.")
         }
     });
+});
+*/
+$(() => {
+    const validateRequiredFieldsSetup = () => {
+        const injectStyle = () => {
+            $('<style id="js-style">').text(`
+                .error-text { color:#ff0000; margin-left: 10px; font-style: italic; }
+                .error-input { outline: 1px solid #ff0000; }
+            `).appendTo('head');
+        };
+        const $submitButton = $('input[type="submit"][name="commit"]');
+        const updateSubmitButton = () => {
+            const anyEmpty = $('.required:text, .required:password')
+            .toArray()
+            .some(function(item) {
+                return $(item).val().trim().length === 0;
+            });
+            $submitButton.prop("disabled", anyEmpty);
+        };
+        $(".input-group").each(function() {
+            const $input = $(this).find(".input-field");
+            const $label = $(this).find(".input-label");
+            if (!$input.hassClass('required')) {
+                if (!$input.is("#email")) {
+                    const validate = () => {
+                        const empty = !$input.val().trim();
+                        if (empty) {
+                            if (!$label.find('.error-text').length) {
+                                $label.append($('<span>', { class: 'error-text', text: ' Required'}));
+                            }
+                            $input.addClass('error-input');
+                        } else {
+                            $label.find('.error-text').remove();
+                            $input.removeClass('error-input');
+                        }
+                        updateSubmitButton();
+                    };
+                    $input.on('input blur', validate);
+                    validate(); // initialize
+                } else {
+                    
+                }
+            }
+        });
+        updateSubmitButton();
+    }
+    const validateEmailSetup = () => {
+        const $email = $("#email");
+    }
 });
