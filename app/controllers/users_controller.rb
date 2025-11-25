d9f976edd3fb10e0e0833039d75f3d6ebbaf5a11
Sullivan Bryant <sbryant1@uiowa.edu>
2025-11-25 10:28:10 -0600

    Fixed routing issues. Successfully stores a user in the persistence field.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -29,9 +29,10 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      #session[:user_id] = @user.id
-      redirect_to root_path, notice: "Welcome, #{@user.username}"
+      session[:user_id] = @user.id
+      redirect_to user_path(@user), notice: "Welcome, #{@user.username}!"
     else
+      Rails.logger.warn "Failed to create an account: #{user.errors.full_messages.to_sentence}"
       render :new, status: :unprocessable_entity
     end
   end
--- config/routes.rb
+++ config/routes.rb
@@ -12,5 +12,6 @@ Rails.application.routes.draw do
   post '/login', :to => 'users#authorize'
   delete '/logout', :to => 'users#logout'
   resources :users, only: [:new, :create, :show]
-  
+  post 'users/check_email', :to => "users#check_email"
+  post 'users/check_username', :to => "users#check_username"
 end
