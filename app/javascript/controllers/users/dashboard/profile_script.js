32c3e313b256775f56e4ba83a03db6926f1e6a65
Sullivan Bryant <sbryant1@uiowa.edu>
2025-11-27 15:42:56 -0600

    (wip) Moved sidebar to a partial to DRY out code.

--- app/javascript/controllers/users/dashboard/profile_script.js
+++ app/javascript/controllers/users/dashboard/profile_script.js
@@ -1,14 +1,12 @@
 $(() => {
-    const $profileDropdown = $("#profile-dropdown-menu");
-    const $profileLink = $("#profile-link");
-    if (!$profileDropdown.length || !$profileLink.length) return;
     $(document).on("click", (evt) => {
+        const $profileDropdown = $("#profile-dropdown-menu");
+        const $profileLink = $("#profile-link");
+        if (!$profileDropdown.length || !$profileLink.length) return;
         if ($profileDropdown.attr('open')) {
             if (!$profileLink.has(evt.target).length) {
                 $profileDropdown.removeAttr("open");
             }
         }
-        evt.bubbles = true;
     });
-    
 });
