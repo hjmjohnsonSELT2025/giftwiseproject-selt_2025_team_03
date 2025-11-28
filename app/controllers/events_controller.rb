898383464db0b50229622480a312240ab57dc2f7
Blake Kellison <bkellison@uiowa.edu>
2025-11-28 11:26:11 -0600

    Add basic functionality for creating events with basic form

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,4 +1,26 @@
 class EventsController < ApplicationController
   before_action :require_authorization
 
+  def index
+    @events = current_user.events.order(:date)
+  end
+
+  def new
+    @event = current_user.events.new
+  end
+
+  def create
+    @event = current_user.events.new(event_params)
+    if @event.save
+      redirect_to events_path, notice: "Event created successfully!"
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def event_params
+    params.require(:event).permit(:name, :date, :location, :theme, :budget)
+  end
 end
