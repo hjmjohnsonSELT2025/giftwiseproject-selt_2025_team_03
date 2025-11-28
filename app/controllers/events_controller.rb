900d2fd581a95c05dfe5cb1d5c034e9af0b78afe
Blake Kellison <bkellison@uiowa.edu>
2025-11-28 14:51:34 -0600

    Add recipient selection to event creation, added recipient checkbox list with association handling. Scrollable selector with custom scrollbar. Shows empty state when no recipients exist

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -7,13 +7,22 @@ class EventsController < ApplicationController
 
   def new
     @event = current_user.events.new
+    @recipients = current_user.recipients.order(:name)
   end
 
   def create
     @event = current_user.events.new(event_params)
+
     if @event.save
+      if params[:recipient_ids].present?
+        params[:recipient_ids].each do |recipient_id|
+          @event.event_recipients.create(recipient_id: recipient_id)
+        end
+      end
+
       redirect_to events_path, notice: "Event created successfully!"
     else
+      @recipients = current_user.recipients.order(:name)
       render :new, status: :unprocessable_entity
     end
   end
