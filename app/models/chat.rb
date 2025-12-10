5c1b4ae562c7820bf4a416fea0c8d9ffb21a9989
Molly-M-Laugh <molly-m-mclaughlin@uiowa.edu>
2025-12-09 21:39:39 -0600

    (wip) Trying to get access to openrouter free models, as denying access yet have 50 chats a day free

--- app/models/chat.rb
+++ app/models/chat.rb
@@ -5,4 +5,9 @@ class Chat < ApplicationRecord
   belongs_to :event_recipient, optional:true
 
   validates :user, presence: true
+
+  def ask(user_input)
+    llm_chat = RubyLLM.chat(provider: :openrouter, model: "deepseek/deepseek-chat-v3-0324:free")
+    llm_chat.ask(user_input)
+  end
 end
